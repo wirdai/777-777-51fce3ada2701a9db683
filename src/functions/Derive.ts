513318dcb0415c18a5cb36c2eab0f8c5c268b332
srdennywird <146856607+srdennywird@users.noreply.github.com>
2023-12-29 10:50:37 -0600

    Update Derive.ts

--- src/functions/Derive.ts
+++ src/functions/Derive.ts
@@ -10,7 +10,7 @@ const Derive: ActivityHandler = async function (
     input: any,
     context: InvocationContext,
 ) {
-    context.log("Enviando mensaje-------------------------------------------------", msg)
+    context.log("Enviando mensaje-------------------------------------------------", input)
     const serviceBusClient = new ServiceBusClient(connectionString);
     const sender = serviceBusClient.createSender(topicName);
 
